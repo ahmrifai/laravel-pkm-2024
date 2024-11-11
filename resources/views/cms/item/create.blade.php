7fdd816c89454b2fec0661929b786495edd184ff
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2024-11-11 07:01:28 +0700

    fixing navbar

--- resources/views/cms/item/create.blade.php
+++ resources/views/cms/item/create.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.theme')
 @section('content')
+<div class="d-flex">
+
     <form action="">
         <input type="text" class="form-control" placeholder="name@example.com"/>
         <input type="text" class="form-control" placeholder="name@example.com"/>
         <input type="text" class="form-control" placeholder="name@example.com"/>
     </form>
+</div>
 @endsection
